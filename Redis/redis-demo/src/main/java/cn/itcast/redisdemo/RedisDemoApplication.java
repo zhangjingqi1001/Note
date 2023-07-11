5d5434b983307403b3f8a4d30406657391e74bc5
zhangjingqi <zhangjingqi@qq.com>
2023-07-11 20:12:00 +0800

    Redis - 哨兵机制完成

---
+++ Redis/redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
@@ -0,0 +1,24 @@
+package cn.itcast.redisdemo;
+
+import io.lettuce.core.ReadFrom;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+
+@SpringBootApplication
+public class RedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDemoApplication.class, args);
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer() {
+//      是一个接口，不能直接new
+        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+    }
+
+
+}
